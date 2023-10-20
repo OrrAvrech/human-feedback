c49893231522761d71a0afeeadf5b43b5d75cad4
Orr Avrech <orravrech@gmail.com>
2023-10-20 14:09:01 -0400

    accumulate_text_by_sentiment.py works well using chatgpt

--- scripts/accumulate_text_by_sentiment.py
+++ scripts/accumulate_text_by_sentiment.py
@@ -1,37 +1,89 @@
 from pathlib import Path
 from utils import read_text
+from typing import NamedTuple
+
+
+class Sentiment(NamedTuple):
+    positive = "Positive"
+    neutral = "Neutral"
+    negative = "Negative"
 
 
 def main():
-    sentiment_paragraphs = [
-        "Alright, number one is the second upward-facing dog. This is a really, really common one because typically, yogis are a little bit tired, so you notice when I come up into my upward-facing dog, my thighs are touching the ground, my arch is on the way back. Not quite exactly how it should be.",
-        "Yeah, so don't do that. Instead, try it this way. When you're going into your forward fold, going through your citation, you come down to your high plank, take your chaturanga, inhale, upward-facing dog. Notice how everything is engaged."
-    ]
-
-    text_dir = Path(
-        "/Users/orrav/Documents/Data/human-feedback/raw/text"
-    )
+    text_dir = Path("/Users/orrav/Documents/Data/human-feedback/raw/text")
     for text_path in text_dir.rglob("*.json"):
         data = read_text(text_path)
-        text_segments = [segment["text"] for segment in data]
+        text_segments = [segment["text"] for segment in data][:18]
+        sentiments = """Positive
+                        Positive
+                        Positive
+                        Neutral
+                        Negative
+                        Positive
+                        Positive
+                        Positive
+                        Positive
+                        Positive
+                        Negative
+                        Neutral
+                        Negative
+                        Negative
+                        Negative
+                        Positive
+                        Positive
+                        Positive""".replace(" ", "").split("\n")
         text_corpus = "".join(text_segments)
 
         samples = []
-        for paragraph in sentiment_paragraphs:
-            text_paragraph = ""
-            start = 0
-            end = None
-            for i, current_segment in enumerate(text_segments):
-                if current_segment.replace(" ", "") in paragraph.replace(" ", ""):
-                    if start == 0:
-                        start = data[i]["timestamp"][0]
-                    end = data[i]["timestamp"][-1]
-                    if end is None:
-                        end = data[i]["timestamp"][0]
-                    text_paragraph += current_segment
+        end = None
+        text_paragraph = text_segments[0]
+        accumulated_sentiments = [sentiments[0]]
+        start = data[0]["timestamp"][0]
+        for i in range(1, len(text_segments)):
+            curr_segment = text_segments[i]
+            curr_sentiment = sentiments[i]
+            prev_sentiment = sentiments[i - 1]
 
-            sample = {"timestamp": [start, end], "text": text_paragraph}
-            samples.append(sample)
+            if (
+                curr_sentiment == prev_sentiment
+                or curr_sentiment == Sentiment.neutral
+            ):
+                text_paragraph += curr_segment
+                accumulated_sentiments.append(curr_sentiment)
+                end = data[i]["timestamp"][-1]
+                if end is None:
+                    end = data[i]["timestamp"][0]
+
+            else:
+                sentiment = Sentiment.positive
+                if Sentiment.negative in accumulated_sentiments:
+                    sentiment = Sentiment.negative
+
+                sample = {
+                    "timestamp": [start, end],
+                    "text": text_paragraph,
+                    "sentiment": sentiment,
+                }
+                samples.append(sample)
+                start = data[i]["timestamp"][0]
+                end = data[i]["timestamp"][-1]
+                text_paragraph = text_segments[i]
+                accumulated_sentiments = [sentiments[i]]
+
+        if Sentiment.positive in accumulated_sentiments:
+            sentiment = Sentiment.positive
+        elif Sentiment.negative in accumulated_sentiments:
+            sentiment = Sentiment.negative
+        else:
+            return
+
+        sample = {
+            "timestamp": [start, end],
+            "text": text_paragraph,
+            "sentiment": sentiment,
+        }
+        samples.append(sample)
+        print(samples)
 
 
 if __name__ == "__main__":
