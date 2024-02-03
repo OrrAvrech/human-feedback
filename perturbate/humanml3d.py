097dc23a06cf2c250e0039b34d50ae5178e85776
Orr Avrech <orravrech@gmail.com>
2024-02-03 13:40:49 -0500

    reorg in perturbation experimental scripts

--- perturbate/humanml3d.py
+++ perturbate/humanml3d.py
@@ -3,7 +3,13 @@ import torch
 import typer
 from pathlib import Path
 from data.datasets import BaseMotionSplit, BaseMotion
-from data.humanml.motion_process import recover_from_ric, extract_features, FID_R, FID_L, FACE_JOINT_INDX
+from data.humanml.motion_process import (
+    recover_from_ric,
+    extract_features,
+    FID_R,
+    FID_L,
+    FACE_JOINT_INDX,
+)
 from data.humanml.kinematic_trees import T2M_KINEMATIC_CHAIN, T2M_RAW_OFFSETS
 from models.smpl import get_smpl_model
 from perturbate.transforms import random_motion_warping
@@ -13,8 +19,15 @@ from concurrent.futures import ThreadPoolExecutor
 import logging
 
 
-def save_random_perturbations(motion_file_list: list[Path], num_joints: int, parents: np.array,
-                              num_iter: int, human_feedback_dir: Path, max_frames: Optional[int] = None):
+def save_random_perturbations(
+    motion_file_list: list[Path],
+    num_joints: int,
+    parents: np.array,
+    num_iter: int,
+    human_feedback_dir: Path,
+    pert_perc: float,
+    max_frames: Optional[int] = None,
+):
     for i, npy_path in enumerate(motion_file_list):
         try:
             motion = torch.Tensor(np.load(npy_path)[:max_frames, ...])
@@ -23,12 +36,19 @@ def save_random_perturbations(motion_file_list: list[Path], num_joints: int, par
             output_dir = human_feedback_dir / npy_path.stem
             output_dir.mkdir(exist_ok=True)
             for i in range(num_iter):
-                posed_joint_positions, _ = random_motion_warping(joints, parents, pert_perc=0.03)
+                posed_joint_positions, _ = random_motion_warping(
+                    joints, parents, pert_perc=pert_perc
+                )
                 posed_joints_np = posed_joint_positions.numpy()
-                posed_data = extract_features(posed_joints_np, feet_thre=0.002,
-                                            n_raw_offsets=torch.from_numpy(T2M_RAW_OFFSETS),
-                                            kinematic_chain=T2M_KINEMATIC_CHAIN,
-                                            fid_r=FID_R, fid_l=FID_L, face_joint_indx=FACE_JOINT_INDX)
+                posed_data = extract_features(
+                    posed_joints_np,
+                    feet_thre=0.002,
+                    n_raw_offsets=torch.from_numpy(T2M_RAW_OFFSETS),
+                    kinematic_chain=T2M_KINEMATIC_CHAIN,
+                    fid_r=FID_R,
+                    fid_l=FID_L,
+                    face_joint_indx=FACE_JOINT_INDX,
+                )
                 output_filepath = output_dir / f"{npy_path.stem}_{i}.npy"
                 np.save(output_filepath, posed_data)
                 print(f"save file {str(output_filepath)}")
@@ -36,34 +56,49 @@ def save_random_perturbations(motion_file_list: list[Path], num_joints: int, par
             logging.error(f"Exception {e} in file {npy_path.name}")
 
 
-def main(dataset_dir: Path, smpl_dir: Path, max_frames: Optional[int] = None,
-         num_joints: Optional[int] = 22, num_iter: Optional[int] = 3, workers: Optional[int] = 1):
+def main(
+    dataset_dir: Path,
+    smpl_dir: Optional[Path] = None,
+    max_frames: Optional[int] = None,
+    pert_perc: Optional[float] = 0.03,
+    num_joints: Optional[int] = 22,
+    num_iter: Optional[int] = 3,
+    workers: Optional[int] = 1,
+):
     human_feedback_dir = dataset_dir / "human_feedback" / "vecs_11"
     human_feedback_dir.mkdir(exist_ok=True, parents=True)
 
-    # ds = BaseMotionSplit(dataset_dir=dataset_dir,
-    #                      motion_dir="new_joint_vecs",
-    #                      split="train",
-    #                      max_frames=max_frames)
-    ds = BaseMotion(dataset_dir=dataset_dir / "new_joint_vecs",
-                    max_frames=max_frames)
+    ds = BaseMotionSplit(
+        dataset_dir=dataset_dir,
+        motion_dir="new_joint_vecs",
+        split="train",
+        max_frames=max_frames,
+    )
     motion_file_list = ds.motion_file_list
     smpl_model = get_smpl_model(smpl_dir)
     parents = smpl_model.parents[:num_joints]
 
     motions_per_worker = (len(motion_file_list) + workers - 1) // workers
     sub_lists = [
-        ds[i : i + motions_per_worker]
+        motion_file_list[i : i + motions_per_worker]
         for i in range(0, len(motion_file_list), motions_per_worker)
     ]
 
     with ThreadPoolExecutor(max_workers=workers) as executor:
         for sublist in sub_lists:
-            executor.submit(save_random_perturbations, sublist, num_joints, parents,
-                            num_iter, human_feedback_dir, max_frames)
+            executor.submit(
+                save_random_perturbations,
+                sublist,
+                num_joints,
+                parents,
+                num_iter,
+                human_feedback_dir,
+                pert_perc,
+                max_frames,
+            )
 
 
 if __name__ == "__main__":
-    logging.basicConfig(filename='threads_log.txt', level=logging.ERROR)
+    logging.basicConfig(filename="threads_log.txt", level=logging.ERROR)
 
     typer.run(main)
--- perturbate/random_edit.py
+++ perturbate/random_edit.py
@@ -3,15 +3,21 @@ import torch
 import joblib
 from pathlib import Path
 from typing import Optional
-from perturbate.transforms import get_smpl_output, random_motion_warping
+from models.smpl import get_smpl_model, get_smpl_output
+from utils.rotation_conversions import axis_angle_to_matrix
+from perturbate.transforms import random_motion_warping
 from utils.visualize import plot_3d_motion, viz_smplx, plot_3d_meshes
-from data.humanml.kinematic_trees import T2M_KINEMATIC_CHAIN
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def load_4d_humans(pkl_path: Path, smpl_dir: Path, num_joints: int, 
-                   max_frames: int, transform: bool = False):
+def load_4d_humans(
+    pkl_path: Path,
+    smpl_dir: Path,
+    num_joints: int,
+    max_frames: int,
+    transform: bool = False,
+):
     data4d = joblib.load(pkl_path)
     predictions = list(data4d.values())
 
@@ -20,7 +26,7 @@ def load_4d_humans(pkl_path: Path, smpl_dir: Path, num_joints: int,
     parents = smpl_model.parents[:num_joints]
     faces = smpl_model.faces
 
-    positions_np  = positions.detach().cpu().numpy()
+    positions_np = positions.detach().cpu().numpy()
     positions_np = positions_np[:max_frames, ...]
     vertices_np = smpl_output.vertices.detach().cpu().numpy()
     vertices_np = vertices_np[:max_frames, ...]
@@ -30,9 +36,10 @@ def load_4d_humans(pkl_path: Path, smpl_dir: Path, num_joints: int,
 
 def main():
     smpl_dir = Path("./models/data")
-    humanml_dir = Path("/Users/orrav/Documents/Data/human-feedback/pose_data_raw/joints")
+    humanml_dir = Path(
+        "/Users/orrav/Documents/Data/human-feedback/pose_data_raw/joints"
+    )
     save_dir = Path("/Users/orrav/Documents/Data/human-feedback/experiments")
-    save_dir = save_dir / "random_pert"
     save_dir.mkdir(exist_ok=True)
 
     # 4D-Humand output
@@ -44,47 +51,34 @@ def main():
     # )
 
     # amass_data -> pose_data (includes HumanML3D transformations)
-    pose_data_path = Path("/Users/orrav/Documents/Data/human-feedback/pose_data_raw/pose_data/00008/misc_poses.npy")
+    pose_data_path = Path(
+        "/Users/orrav/Documents/Data/human-feedback/pose_data_raw/pose_data/00008/misc_poses.npy"
+    )
     # HumanML3D joint before motion_process.py
     joints_path = Path("/Users/orrav/Documents/Data/HumanML3D/joints/000000.npy")
     # HumanML3D after motion_process.py
-    new_joints_path = Path("/Users/orrav/Documents/Data/human-feedback/pose_data_raw/new_joints/demo_10_most_common_YOGA_MISTAKES_new_students_make_and_how_to_fix_them_124_euler.npy")
+    new_joints_path = Path(
+        "/Users/orrav/Documents/Data/human-feedback/pose_data_raw/new_joints/demo_10_most_common_YOGA_MISTAKES_new_students_make_and_how_to_fix_them_124_euler.npy"
+    )
     # SMPLify path after MDM
-    smplify_path = Path("/Users/orrav/Documents/Data/human-feedback/experiments/edit_humanml_trans_enc_512_000200000_in_between_seed10_a_person_bends_his_knees_slightly/sample01_rep00_smpl_params.npy")
-    a = np.load(smplify_path, allow_pickle=True)
+    smplify_path = Path(
+        "/Users/orrav/Documents/Data/human-feedback/experiments/edit_humanml_trans_enc_512_000200000_in_between_seed10_a_person_bends_his_knees_slightly/sample01_rep00_smpl_params.npy"
+    )
+    # a = np.load(smplify_path, allow_pickle=True)
+    # MOYO examples
+    moyo_path = Path(
+        "/Users/orrav/Documents/Data/moyo_toolkit/data/mosh_smpl/val/221004_yogi_nexus_body_hands_03596_Upward_Plank_Pose_or_Purvottanasana_-a_stageii_smpl.pkl"
+    )
 
     save_npy = False
     # HumanML3D for MDM
     num_joints = 22
-    max_frames = 481
+    max_frames = None
 
-    new_joints = np.load(new_joints_path)
+    # transform = True
+    # positions, vertices, parents, faces = load_4d_humans(pkl_path, smpl_dir, num_joints, max_frames,
+    #                                                      transform=transform)
 
-    transform = True
-    positions, vertices, parents, faces = load_4d_humans(pkl_path, smpl_dir, num_joints, max_frames,
-                                                         transform=transform)
-    
-    num_iter = 3
-    rep_files = []
-
-    ani_save_path = save_dir / f"{pkl_path.stem}_input.mp4"
-    plot_3d_motion(ani_save_path, T2M_KINEMATIC_CHAIN, positions, title="input-motion", fps=40)
-
-    rep_files.append(ani_save_path)
-    for i in range(num_iter):
-        posed_joint_positions, pert_frames = random_motion_warping(torch.Tensor(positions), parents, pert_perc=0.01)
-        posed_joints_np = posed_joint_positions.numpy()
-        ani_save_path = save_dir / f"{pkl_path.stem}_random_{i}.mp4"
-        plot_3d_motion(ani_save_path, T2M_KINEMATIC_CHAIN, positions, title=f"iter-{i}", fps=40, pert_frames=pert_frames)
-        print(f"saved animation in {ani_save_path}")
-        rep_files.append(ani_save_path)
-
-    all_rep_save_file = save_dir / "output.mp4"
-    ffmpeg_rep_files = [f' -i {str(f)} ' for f in rep_files]
-    hstack_args = f' -filter_complex hstack=inputs={num_iter+1}'
-    ffmpeg_rep_cmd = f'ffmpeg -y -loglevel warning ' + ''.join(ffmpeg_rep_files) + f'{hstack_args} {all_rep_save_file}'
-    os.system(ffmpeg_rep_cmd)
-    
     # vertices = a.item()["vertices"].permute(2, 0, 1).numpy()
 
     # Save SMPL joints from 4DHumans to be converted to HumanML3D
@@ -93,10 +87,36 @@ def main():
     #     npy_path = humanml_dir / f"{pkl_path.stem}_euler.npy"
     #     np.save(npy_path, positions)
 
-    # ani_save_path = save_dir / f"{pkl_path.stem}_random_{i}.mp4"
+    moyo_smpl = np.load(moyo_path, allow_pickle=True)
+
+    smpl_params = dict()
+    smpl_params["global_orient"] = axis_angle_to_matrix(
+        torch.Tensor(moyo_smpl["global_orient"])
+    ).unsqueeze(1)
+    smpl_params["body_pose"] = axis_angle_to_matrix(
+        torch.Tensor(moyo_smpl["body_pose"]).reshape(-1, 23, 3)
+    )
+    smpl_params["betas"] = torch.Tensor(moyo_smpl["betas"])[:, :10]
+    smpl_params["transl"] = torch.Tensor(moyo_smpl["transl"])
+
+    smpl_model = get_smpl_model(smpl_dir)
+    faces = smpl_model.faces
+    smpl_output = smpl_model(**smpl_params, pose2rot=False)
+
+    positions = smpl_output.joints.detach().cpu().numpy()
+    positions = positions[:max_frames, ...]
+    vertices = smpl_output.vertices.detach().cpu().numpy()
+    vertices = vertices[:max_frames, ...]
+
+    trans_matrix = np.array([[1.0, 0.0, 0.0], [0.0, 0.0, 1.0], [0.0, 1.0, 0.0]])
+    positions = np.dot(positions, trans_matrix)
+
+    ani_save_path = save_dir / f"{moyo_path.stem}.mp4"
 
     # plot_3d_meshes(ani_save_path, positions, vertices, faces, T2M_KINEMATIC_CHAIN, fps=40)
-    # plot_3d_motion(ani_save_path, T2M_KINEMATIC_CHAIN, positions, title="SMPL2HumanML", fps=40)
+    plot_3d_motion(
+        ani_save_path, positions, title="SMPL2HumanML", fps=40
+    )
     # print(f"saved animation in {ani_save_path}")
 
 
@@ -105,7 +125,7 @@ if __name__ == "__main__":
     # Given HumanML3D dataset object
     # Iterate over the dataset
     # for each motion (or a batch of motions):
-        # create 10 (param) pertubed samples from random transformations
-        # convert back to humanml representation
-        # save in directory "random_perturbation" with subdirs of each filename
+    # create 10 (param) pertubed samples from random transformations
+    # convert back to humanml representation
+    # save in directory "random_perturbation" with subdirs of each filename
     # do the same for MDM with randomized text prompts
